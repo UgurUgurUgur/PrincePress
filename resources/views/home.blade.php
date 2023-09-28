c11b30f36fab7a0a113c58df5ebfabbdb05f3561
UgurUgurUgur <ugurkankaraca03@gmail.com>
2023-09-28 09:37:28 +0200

    - updated controllers - hosting now works - replaced layout.blade.php with app.blade.php

---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+@extends('app')
+
+@section('content')
+<div class="releases">
+    <h2>Releases</h2>
+    <!-- releases -->
+</div>
+
+<div class="coming-soon">
+    <h2>Coming Soon</h2>
+    <!-- coming soon -->
+</div>
+
+<div class="all-series">
+    <h2>All Series</h2>
+    <!-- all series -->
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -2,24 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\NavController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/', [HomeController::class, 'index'])->name('home');
-Route::get('/series', [HomeController::class, 'series'])->name('series');
-Route::get('/short-stories', [HomeController::class, 'shortStories'])->name('short-stories');
-Route::get('/about', [HomeController::class, 'about'])->name('about');
-Route::get('/contact', [HomeController::class, 'contact'])->name('contact');
-
+Route::get('/', [NavController::class, 'index'])->name('home');
+Route::get('/series', [NavController::class, 'series'])->name('series');
+Route::get('/short-stories', [NavController::class, 'shortStories'])->name('short-stories');
+Route::get('/about', [NavController::class, 'about'])->name('about');
+Route::get('/contact', [NavController::class, 'contact'])->name('contact');
