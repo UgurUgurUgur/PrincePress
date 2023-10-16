2f4012a7071c762893464a84f4fec723d2e71d5c
UgurUgurUgur <ugurkankaraca03@gmail.com>
2023-10-16 14:51:55 +0200

    updaed home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,25 +4,8 @@
 <div class="video-banner-container">
     <video autoplay loop muted playsinline class="video-banner" width="100%" height="50%">
         <source src="{{ asset('videos/PrincePressBanner.mp4') }}" type="video/mp4">
-        Your browser does not support the video tag.
+        <p>Our stories are as unique as the contrast of black words on white sheets of paper.</p>
     </video>
-    <div class="video-overlay">
-        <!-- Navigation Menu -->
-        <nav class="overlay-menu">
-            <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">Releases</a></li>
-                <li><a href="#">Coming Soon</a></li>
-                <li><a href="#">All Series</a></li>
-            </ul>
-        </nav>
-
-        <!-- Prince.png Image -->
-        <img src="{{ asset('images/Prince.png') }}" alt="Prince" class="overlay-image prince">
-
-        <!-- Press.png Image -->
-        <img src="{{ asset('images/Press.png') }}" alt="Press" class="overlay-image press">
-
         <div class="releases">
             <h2>Releases</h2>
             <!-- releases -->
